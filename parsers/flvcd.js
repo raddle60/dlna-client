30edababa97586ee08c5b0f9a7b24e587752f872
raddle60 <raddle60@users.noreply.github.com>
2014-09-08 15:49:28 +0800

    配置

---
+++ parsers/flvcd.js
@@ -0,0 +1,47 @@
+/**
+* 解析器信息
+**/
+var parserInfo = {
+	name : "FLVCD硕鼠",
+	qualitys : [{key:"super",value:"超清"},{key:"high",value:"高清"}]
+};
+
+/**
+* url转换
+**/
+function fetchVideoUrls(url,videoQuality){
+    var videoInfo = innerFetchVideoUrls(url,videoQuality);
+    // 为空再试一次，经常地再一次请求会失败
+    if(videoInfo == null){
+        videoInfo = innerFetchVideoUrls(url,videoQuality);
+    }
+    return videoInfo;
+}
+
+function innerFetchVideoUrls(url,videoQuality){
+    var parseUrl = "http://www.flvcd.com/parse.php?kw=" + httpclient.encodeUrl(url,"GBK") + "&flag=one&format=" + videoQuality;
+    var content = httpclient.getRemotePage(parseUrl,"GBK",null);
+    var videoUrlRegex =  new RegExp("<input type=\"hidden\" name=\"inf\" value=\"([^\"]+)\"/>","g");
+    var matchedUrlHidden = content.match(videoUrlRegex);
+    var videoInfo = {};
+    // url地址
+    if(matchedUrlHidden != null && matchedUrlHidden.length > 0){
+        var urlstr = matchedUrlHidden[0].match(new RegExp("value=\"([^\"]+)\"/>"))[1];
+        if(urlstr.charAt(urlstr.length - 1) == '|'){
+            urlstr=urlstr.substring(0,urlstr.length - 1);
+        }
+        var urls = urlstr.split("|");
+        videoInfo.urls = urls;
+    }
+    // 视频名称
+    var videoNameRegex =  new RegExp("<input type=\"hidden\" name=\"filename\" value=\"([^\"]+)\"/>","g");
+    var videoNameHidden = content.match(videoNameRegex);
+    if(videoNameHidden != null && videoNameHidden.length > 0){
+        var videoName = videoNameHidden[0].match(new RegExp("value=\"([^\"]+)\"/>"))[1];
+        videoInfo.name = videoName;
+    }
+    if(videoInfo.urls){
+        return videoInfo;
+    }
+    return null;
+}
