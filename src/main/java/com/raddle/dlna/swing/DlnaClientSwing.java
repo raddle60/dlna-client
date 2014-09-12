ce4ab64c214549aa7d8c1c18cc0b76dd1a890065
raddle60 <raddle60@users.noreply.github.com>
2014-09-12 21:05:17 +0800

    增加检查延迟

--- src/main/java/com/raddle/dlna/swing/DlnaClientSwing.java
+++ src/main/java/com/raddle/dlna/swing/DlnaClientSwing.java
@@ -707,7 +707,7 @@ public class DlnaClientSwing {
 					@Override
 					public void run() {
 						try {
-							Thread.sleep(500);
+							Thread.sleep(1000);
 						} catch (InterruptedException e1) {
 							return;
 						}
